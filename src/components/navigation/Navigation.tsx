d6d68b414ba2ffbce09065ea244029eb2200f5d8
DadsDoingDesign <179973449+DadsDoingDesign@users.noreply.github.com>
2025-01-28 00:09:07 -0500

    navigation reanimation fix

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -27,6 +27,8 @@ const Navigation = ({ className = '', isViewingCaseStudy = false, onBack }: Navi
 
   return (
     <motion.div 
+      layout
+      initial={{ opacity: 1 }}
       className={`w-full flex justify-between items-center px-8 py-4 ${className}`}
     >
       {isViewingCaseStudy ? (
